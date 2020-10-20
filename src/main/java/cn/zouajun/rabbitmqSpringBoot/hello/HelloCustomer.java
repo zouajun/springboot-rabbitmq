2cc02d2d22adb6cb230c6b3b53a4065df5de5cee
zouajun <1293848481@qq.com>
2020-10-20 17:07:03 +0800

    commit

---
+++ src/main/java/cn/zouajun/rabbitmqSpringBoot/hello/HelloCustomer.java
@@ -0,0 +1,17 @@
+package cn.zouajun.rabbitmqSpringBoot.hello;
+
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queuesToDeclare = @Queue("hello"))
+public class HelloCustomer {
+
+    @RabbitHandler
+    public void receive(String message){
+        System.out.println("message" + message);
+    }
+
+}
